2a455aabff3698fcea66a5debd7c9f251ad1afd9
KB <kbm0996@naver.com>
2019-08-16 02:23:22 +0900

    rework example

--- MySQL_Example/MySQL_Example/_main.cpp
+++ MySQL_Example/MySQL_Example/_main.cpp
@@ -2,55 +2,64 @@
 #include "mysql/include/mysql.h"
 #include <cstdio>
 
+#define df_DB_IP "127.0.0.1"
+#define df_DB_ID "root"
+#define df_DB_PASS "1234"
+#define df_DB_NAME "game_schema"
+#define df_DB_PORT 3306
+
+
 int main()
 {
-	MYSQL conn;
-	MYSQL *connection = NULL;
-	MYSQL_RES *sql_result;
-	MYSQL_ROW sql_row;
+	MYSQL mysql;
+	MYSQL_RES* sqlResultSet;
+	MYSQL_ROW sqlRow;
 	int query_stat;
 
-	// 초기화
-	mysql_init(&conn);
+	// * mysql_init() : 초기화
+	mysql_init(&mysql);
 
-	// DB 연결
-	connection = mysql_real_connect(&conn, "127.0.0.1", "root", "1234", "game_schema", 3306, (char *)NULL, 0);
-	if (connection == NULL)
+	// * mysql_real_connect() : DB 연결
+	if (!mysql_real_connect(&mysql, df_DB_IP, df_DB_ID, df_DB_PASS, df_DB_NAME, df_DB_PORT, NULL, 0))
 	{
-		fprintf(stderr, "Mysql connection error : %s", mysql_error(&conn));
+		// * mysql_errno(), mysql_error() : DB 에러 표시
+		// - fprintf() : file printf. 서식있는 출력(printf에 파일 포인터가 인자로 추가)
+		// - stdin : 표준 입력 장치(default keyboard)
+		// - stdout : 표준 출력 장치(default monitor)
+		// - stderr : 표준 에러 출력 장치(default monitor)
+		fprintf(stderr, "mysql_real_connect() error : %d %s", mysql_errno(&mysql), mysql_error(&mysql));
 		return 1;
 	}
 
-	//한글사용을위해추가.
-	mysql_set_character_set(connection, "utf8");
+	// * mysql_set_character_set() : mysql_real_escape_string()의 캐릭터셋 설정
+	//mysql_set_character_set(&mysql, "utf8");
 
-
-	//	mysql_query(connection,"set session character_set_connection=euckr;");
-	//	mysql_query(connection,"set session character_set_results=euckr;");
-	//	mysql_query(connection,"set session character_set_client=euckr;");
-
-
-	// Select 쿼리문
-	char *query = "SELECT * FROM account";	// From 다음 DB에 존재하는 테이블 명으로 수정하세요
-	query_stat = mysql_query(connection, query);
-	if (query_stat != 0)
+	// * mysql_query() : 쿼리 실행
+	// - character_set_connection : 쿼리문에서 인트로듀서가 없는 리터럴 또는 숫자를 문자열로 변환할 때 사용하는 캐릭터셋
+	// - character_set_results : MySQL 서버가 쿼리 처리 결과를 클라이언트로 보낼 때 사용하는 캐릭터셋
+	// - character_set_client : MySQL 클라이언트의 캐릭터셋
+	mysql_query(&mysql,"set session character_set_connection=euckr;");
+	mysql_query(&mysql,"set session character_set_results=euckr;");
+	mysql_query(&mysql,"set session character_set_client=euckr;");
+	if (mysql_query(&mysql, "SELECT * FROM account") != 0)
 	{
-		printf("Mysql query error : %s", mysql_error(&conn));
+		fprintf(stderr, "mysql_query() error : %d %s", mysql_errno(&mysql), mysql_error(&mysql));
 		return 1;
 	}
 
-	// 결과출력
-	sql_result = mysql_store_result(connection);		// 결과 전체를 미리 가져옴
-														//	sql_result=mysql_use_result(connection);		// fetch_row 호출시 1개씩 가져옴
-
-	while ((sql_row = mysql_fetch_row(sql_result)) != NULL)
+	// * mysql_store_result(), mysql_use_result() : mysql_query() 실행 성공 시 결과를 출력하기 위해 결과셋에 저장
+	// * mysql_fetch_row() : 레코드를 가져오기(fetch). 검색 row가 없거나 에러 발생 시 NULL 리턴
+	sqlResultSet = mysql_store_result(&mysql);	
+	while ((sqlRow = mysql_fetch_row(sqlResultSet)) != NULL)
 	{
-		printf("%2s %2s %s\n", sql_row[0], sql_row[1], sql_row[2]);
+		printf("%s %s %s\n", sqlRow[0], sqlRow[1], sqlRow[2]);
 	}
-	mysql_free_result(sql_result);
 
-	// DB 연결닫기
-	mysql_close(connection);
+	// * mysql_free_result() : 결과셋에 할당된 메모리 해제
+	mysql_free_result(sqlResultSet);
+
+	// * mysql_close() : DB 세션 닫기
+	mysql_close(&mysql);
 
 	return 0;
 }
--- ODBC_Example/ODBC_Example/_main.cpp
+++ ODBC_Example/ODBC_Example/_main.cpp
@@ -1,109 +1,70 @@
 #include <Windows.h>
 #include <cstdio>
 #include <cstdlib>
-#include <sql.h>
-#include <sqlext.h>
 #include <conio.h>
 #include <clocale>
+/* SQL ODBC */
+#include <sql.h>
+#include <sqlext.h>
+#define df_DB_IP L"127.0.0.1"
+#define df_DB_ID L"root"
+#define df_DB_PASS L"1234"
+#define df_DB_NAME L"game_schema"
+#define df_DB_PORT 3306
 
-/////////////////////////////////////////////
-// 핸들 변수 선언
-// ODBC 사용시 앞에 SQLdl 붙은 Type 사용
-/////////////////////////////////////////////
-SQLHENV		hEnv;
-SQLHDBC		hDbc;
-SQLHSTMT	hStmt;
+SQLHENV		g_hEnv;	
+SQLHDBC		g_hDbc;
+SQLHSTMT	g_hStmt;
 
-// 결과값을 받기 위한 변수
-HRESULT		hResult;
+SQLRETURN	MYSQLConnect(const WCHAR * szODBCIP, const WCHAR * szODBCName, const WCHAR * szODBCID, const WCHAR * szODBCPW);
+void		MYSQLDisconnect();
 
 int main()
 {
+	WCHAR		szStat[10];
+	WCHAR		szMsg[512];
+	SQLSMALLINT	iMsglen;
+	int			iNativeErr;
+
 	setlocale(LC_ALL, "");
 
-	WCHAR		State[1024];
-	WCHAR		Message[1024];
-	SQLSMALLINT	MsgLen;
-	int			NativeErr;
-
-	TCHAR		BuffNo[40];
-	TCHAR		BuffID[40];
-	TCHAR		BuffPass[40];
-
-	SQLLEN		lBuffNo, lBuffID, lBuffPass;
-
-	// Binary
-	SQLLEN		Size = SQL_LEN_DATA_AT_EXEC(512);
-	SQLPOINTER	Token;
-
-	/////////////////////////////////////////////////
-	// 환경설정 할당 + 버전속성 설정
-	/////////////////////////////////////////////////
-	if (SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &hEnv) != SQL_SUCCESS) return 0;
-	if (SQLSetEnvAttr(hEnv, SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3, SQL_IS_INTEGER) != SQL_SUCCESS) return 0;
-
-	/////////////////////////////////////////////////
-	// 접속핸들 할당 + 연결
-	/////////////////////////////////////////////////
-	if (SQLAllocHandle(SQL_HANDLE_DBC, hEnv, &hDbc) != SQL_SUCCESS) return 0;
-	// 접속핸들에 대한 에러 메시지 처리
-	ZeroMemory(Message, 1024);
-	MsgLen = 0;
-	SQLGetDiagRec(SQL_HANDLE_DBC, hDbc, 1, (SQLTCHAR*)State, (SQLINTEGER*)&NativeErr, (SQLTCHAR*)Message, 1024, &MsgLen);
-	wprintf(L"%s\n", Message);
-
-	// #1 DSN
-	///SQLConnect(hDbc, (SQLTCHAR*)"Connect GameSchema", SQL_NTS, (SQLTCHAR*)NULL, SQL_NTS, (SQLTCHAR*)NULL, SQL_NTS);
-
-	// #2
-	WCHAR szConnect[1024] = L"Driver={MySQL ODBC 5.3 Unicode Driver}; Server=127.0.0.1; Database=game_schema; User=root; Password=1234; Option=3;";
-	SQLTCHAR OutCon[255];
-	SQLSMALLINT cbOutCon;
-
-	if (SQLDriverConnect(hDbc, NULL, (SQLTCHAR *)szConnect, wcslen(szConnect), OutCon, sizeof(OutCon), &cbOutCon, SQL_DRIVER_COMPLETE)//SQL_DRIVER_NOPROMPT);
-																																	  //SQLDriverConnect(hDbc, NULL, (SQLTCHAR *)szConnect, wcslen(szConnect), OutCon, sizeof(OutCon)/2, &cbOutCon, SQL_DRIVER_NOPROMPT);
-																																	  // 접속에 대한 메세지 처리
-		 > 1)
+	/**********************************************************/
+	// DB 연결
+	/**********************************************************/
+	SQLRETURN iRetval = MYSQLConnect(df_DB_IP, df_DB_NAME, df_DB_ID, df_DB_PASS);
+	if (iRetval != 0)
 	{
-		ZeroMemory(Message, 1024);
-		MsgLen = 0;
-		SQLGetDiagRec(SQL_HANDLE_DBC, hDbc, 1, (SQLTCHAR*)State, (SQLINTEGER*)&NativeErr, (SQLTCHAR*)Message, 1024, &MsgLen);
-		wprintf(L"%s\n", Message);
+		SQLGetDiagRec(SQL_HANDLE_DBC, g_hDbc, 1, (SQLTCHAR*)szStat, (SQLINTEGER*)&iNativeErr, (SQLTCHAR*)szMsg, 1024, &iMsglen);
+		wprintf(L"%s\n", szMsg);
+		return iRetval;
 	}
-	/////////////////////////////////////////////////
-	// 명령핸들 할당 + 실행
-	/////////////////////////////////////////////////
-	if (SQLAllocHandle(SQL_HANDLE_STMT, hDbc, &hStmt) != SQL_SUCCESS) return false;
-
+	
+	/**********************************************************/
+	// 쿼리 실행
+	/**********************************************************/
+	SQLCHAR		BuffNo[40], BuffID[40], BuffPass[40];
+	SQLLEN		lBuffNo = 0, lBuffID = 0, lBuffPass = 0;
 	// 바인딩 (select문에서 데이터 목록을 가져오기 위함 + SQL 인젝션 방지)
-	SQLBindCol(hStmt, 1, SQL_C_TCHAR, BuffNo, 40, (SQLLEN*)&lBuffNo);
-	SQLBindCol(hStmt, 2, SQL_C_TCHAR, BuffID, 40, (SQLLEN*)&lBuffID);
-	SQLBindCol(hStmt, 3, SQL_C_TCHAR, BuffPass, 40, (SQLLEN*)&lBuffPass);
-	ZeroMemory(Message, 1024);
-	MsgLen = 0;
-	SQLGetDiagRec(SQL_HANDLE_STMT, hStmt, 1, (SQLTCHAR*)State, (SQLINTEGER*)&NativeErr, (SQLTCHAR*)Message, 1024, &MsgLen);
-	wprintf(L"%s\n", Message);
-	/*
-	// 실행하기 (INSERT INTO)
-	SQLExecDirect(hStmt, (SQLTCHAR*)"INSERT INTO Member(ID,Pass,Name) VALUES('myID', '2222', 'long'", SQL_NTS);
-	// 접속에 대한 메세지 처리
-	SQLGetDiagRec(SQL_HANDLE_DBC, hDbc, 1, (SQLTCHAR*)State, (SQLINTEGER*)&NativeErr, (SQLTCHAR*)Message, 1024, &MsgLen);
-	wprintf(L"%s\n", Message);
-	*/
+	// * SQLBindCol(명령핸들, 컬럼번호, 해당컬럼의 데이터유형, 버퍼, 버퍼 '길이', )  
+	SQLBindCol(g_hStmt, 1, SQL_C_TCHAR, BuffNo, sizeof(BuffNo), (SQLLEN*)&lBuffNo);
+	SQLBindCol(g_hStmt, 2, SQL_C_TCHAR, BuffID, sizeof(BuffID), (SQLLEN*)&lBuffID);
+	SQLBindCol(g_hStmt, 3, SQL_C_TCHAR, BuffPass, sizeof(BuffPass), (SQLLEN*)&lBuffPass);
 
-	// 실행하기 (SELECT FROM)
-	SQLExecDirect(hStmt, (SQLTCHAR*)L"SELECT * FROM account", SQL_NTS);
-	ZeroMemory(Message, 1024);
-	MsgLen = 0;
-	SQLGetDiagRec(SQL_HANDLE_STMT, hStmt, 1, (SQLTCHAR*)State, (SQLINTEGER*)&NativeErr, (SQLTCHAR*)Message, 1024, &MsgLen);
-	wprintf(L"%s\n", Message);
+	///SQLExecDirect(hStmt, (SQLTCHAR*)"INSERT INTO Member(ID,Pass,Name) VALUES('myID', '2222', 'long'", SQL_NTS);
+	SQLExecDirect(g_hStmt, (SQLTCHAR*)L"SELECT * FROM account", SQL_NTS);;
 
+	/**********************************************************/
 	// 결과 출력
-	while (SQLFetch(hStmt) != SQL_NO_DATA)
+	/**********************************************************/
+	// * 컬럼의 수를 리턴해주는 함수 : 클래스화할때 사용. 성능은 떨어짐
+	//	SQLNumResultCols(hStmt, (int)&컬럼값);
+	// * 컬럼 이름 얻어오기
+	//	SQLDescribeCol(hStmt, 컬럼번호, Buffer, sizeof(Buffer), NULL,NULL,NULL,NULL,NULL)
+	while (SQLFetch(g_hStmt) != SQL_NO_DATA)
 	{
 		wprintf(L"No: %s, ID:%s, Pass:%s\n", BuffNo, BuffID, BuffPass);
-		SQLGetDiagRec(SQL_HANDLE_STMT, hStmt, 1, (SQLTCHAR*)State, (SQLINTEGER*)&NativeErr, (SQLTCHAR*)Message, 1024, &MsgLen);
-		//wprintf(L"%s\n", Message);
+		SQLGetDiagRec(SQL_HANDLE_STMT, g_hStmt, 1, (SQLTCHAR*)szStat, (SQLINTEGER*)&iNativeErr, (SQLTCHAR*)szMsg, 1024, &iMsglen);
+		//wprintf(L"%s\n", szMsg);
 
 		/*
 		// 쿼리 내에서 결과로 다시 쿼리를 날려야하는 경우
@@ -111,8 +72,8 @@ int main()
 		if (SQLAllocHandle(SQL_HANDLE_STMT, hDbc, &hStmt2) != SQL_SUCCESS) return false;
 		hResult = SQLBindCol(hStmt2, 1, SQL_C_CHAR, BuffPass, 8, (SQLLEN*)&lBuffID);
 		hResult = SQLExecDirect(hStmt2, (SQLTCHAR*)"SELECT * FROM tblUser", SQL_NTS);
-		SQLGetDiagRec(SQL_HANDLE_STMT, hStmt2, 1, (SQLTCHAR*)State, (SQLINTEGER*)&NativeErr, (SQLTCHAR*)Message, 1024, &MsgLen);
-		wprintf(L"%s\n", Message);
+		SQLGetDiagRec(SQL_HANDLE_STMT, hStmt2, 1, (SQLTCHAR*)szStat, (SQLINTEGER*)&iNativeErr, (SQLTCHAR*)szMsg, 1024, &iMsglen);
+		wprintf(L"%s\n", szMsg);
 
 		while (SQLFetch(hStmt2) != SQL_NO_DATA)
 		{
@@ -123,29 +84,67 @@ int main()
 		*/
 	}
 
-	// 마무리
-	SQLCloseCursor(hStmt);
-	SQLFreeHandle(SQL_HANDLE_STMT, hStmt);
-	SQLDisconnect(hDbc);
-	SQLFreeHandle(SQL_HANDLE_DBC, hDbc);
-	SQLFreeHandle(SQL_HANDLE_ENV, hEnv);
-
-	/*****************************************************
-	* SQL 에러의 내용을 한글로 자세히 알려주는 함수
-	SQLGetDiagRec(
-	SQL_HANDLE_ENV,
-	DBC,
-	STMT,
-	핸들, 1, (SQLCHAR*)State,
-	(SQLINTEGER*)&NativeErr,
-	(SQLCHAR*)Message, // 에러메세지가 저장되는 버퍼
-	MsgSize, // 버퍼 사이즈
-	&MsgLen) // 버퍼 길이
-	* 컬럼의 수를 리턴해주는 함수 : 클래스화할때 사용. 그러나 성능이 떨어져 잘 사용 X
-	SQLNumResultCols(hStmt, (int)&컬럼값);
-	* 컬럼의 이름을 얻어온다
-	SQLDescribeCol(hStmt, 컬럼번호, Buffer, sizeof(Buffer), NULL,NULL,NULL,NULL,NULL)
-	*****************************************************/
+	/**********************************************************/
+	// DB 연결 해제
+	/**********************************************************/
+	MYSQLDisconnect();
 
 	return 0;
+}
+
+SQLRETURN MYSQLConnect(const WCHAR * szODBCIP, const WCHAR * szODBCName, const WCHAR * szODBCID, const WCHAR * szODBCPW)
+{
+	SQLRETURN iRetval = SQL_SUCCESS;
+
+	////////////////////////////////////////////////////////////////
+	// 핸들 할당
+	//
+	////////////////////////////////////////////////////////////////
+	// 1. 환경 핸들 할당
+	// * SQLAllocHandle() : 환경, 연결, 명령문 핸들 할당
+	if (iRetval = SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &g_hEnv) < 0) // 0 = SQL_SUCCESS, 1 = SQL_SUCCESS_WITH_INFO
+		return iRetval;
+
+	// 2. ODBC 드라이버 버전 설정
+	// * SQLSetEnvAttr() : 환결 제어 특성 설정
+	if (iRetval = SQLSetEnvAttr(g_hEnv, SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3, 0) < 0)
+		return iRetval;
+
+	// 3. 연결 핸들 할당
+	if (iRetval = SQLAllocHandle(SQL_HANDLE_DBC, g_hEnv, &g_hDbc) < 0)
+		return iRetval;
+
+
+	////////////////////////////////////////////////////////////////
+	// DBMS 연결
+	//
+	////////////////////////////////////////////////////////////////
+	// 3. DB 연결 방법 : DSN (only x32)
+	// * SQLConnect() : 연결 핸들로 드라이버 및 데이터 원본에 대한 연결 설정
+	// - ODBC 드라이버 DSN 설정 : 제어판 ~ 관리도구 ~ 데이터원본(ODBC) ~ 사용자 DSN 탭
+	//if (iRetval = SQLConnect(g_hDbc, (SQLTCHAR*)"connect_gameschema", SQL_NTS, (SQLTCHAR*)szODBCID, SQL_NTS, (SQLTCHAR*)szODBCPW, SQL_NTS) < 0)
+	//	return iRetval;
+	
+	// 3. DB 연결 방법 : Driver
+	// * SQLDriverConnect() : 핸들없이 각종 정보를 포함하는 문자열을 사용하여 연결 설정
+	// - ODBC 드라이버명 조회 : 제어판 ~ 관리도구 ~ 데이터원본(ODBC) ~ 드라이버탭
+	WCHAR szConIn[255] = L"\0";
+	swprintf_s(szConIn, L"Driver={MySQL ODBC 5.3 Unicode Driver}; Server=%s; Database=%s; User=%s; Password=%s", szODBCIP, szODBCName, szODBCID, szODBCPW);
+	if (iRetval = SQLDriverConnect(g_hDbc, NULL, szConIn, wcslen(szConIn), NULL, NULL, NULL, SQL_DRIVER_COMPLETE/*SQL_DRIVER_NOPROMPT*/) < 0)
+		return iRetval;
+
+	// 4. 명령 핸들 할당
+	if (iRetval = SQLAllocHandle(SQL_HANDLE_STMT, g_hDbc, &g_hStmt) < 0)
+		return iRetval;
+
+	return SQL_SUCCESS;
+}
+
+void MYSQLDisconnect()
+{
+	SQLCloseCursor(g_hStmt);
+	SQLFreeHandle(SQL_HANDLE_STMT, g_hStmt);
+	SQLDisconnect(g_hDbc);
+	SQLFreeHandle(SQL_HANDLE_DBC, g_hDbc);
+	SQLFreeHandle(SQL_HANDLE_ENV, g_hEnv);
 }
